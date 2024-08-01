26ba3e7a6855fd5250982126bdfdc96c74e6f511
secanty <emt@emt.cc>
2024-08-01 01:20:41 +0000

    . Updated By Github Actions

--- js/fish.js
+++ js/fish.js
@@ -4,7 +4,7 @@ var RENDERER = {
 	MAX_INTERVAL_COUNT : 50,
 	INIT_HEIGHT_RATE : 0.5,
 	THRESHOLD : 50,
-	
+
 	init : function(){
 		this.setParameters();
 		this.reconstructMethods();
@@ -25,11 +25,11 @@ var RENDERER = {
 		var count = Math.round(this.width / this.POINT_INTERVAL);
 		this.pointInterval = this.width / (count - 1);
 		this.points.push(new SURFACE_POINT(this, 0));
-		
+
 		for(var i = 1; i < count; i++){
 			var point = new SURFACE_POINT(this, i * this.pointInterval),
 				previous = this.points[i - 1];
-				
+
 			point.setPreviousPoint(previous);
 			previous.setNextPoint(point);
 			this.points.push(point);
@@ -53,7 +53,7 @@ var RENDERER = {
 		this.fishCount = this.FISH_COUNT * this.width / 500 * this.height / 500;
 		this.$canvas.attr({width : this.width, height : this.height});
 		this.reverse = false;
-		
+
 		this.fishes.push(new FISH(this));
 		this.createSurfacePoints();
 	},
@@ -72,10 +72,10 @@ var RENDERER = {
 		var width = this.$window.width(),
 			height = this.$window.height(),
 			stopped = (width == this.tmpWidth && height == this.tmpHeight);
-			
+
 		this.tmpWidth = width;
 		this.tmpHeight = height;
-		
+
 		if(stopped){
 			this.setup();
 		}
@@ -84,11 +84,11 @@ var RENDERER = {
 		this.$window.on('resize', this.watchWindowSize);
 		this.$container.on('mouseenter', this.startEpicenter);
 		this.$container.on('mousemove', this.moveEpicenter);
-		this.$container.on('click', this.reverseVertical);
+		// this.$container.on('click', this.reverseVertical);
 	},
 	getAxis : function(event){
 		var offset = this.$container.offset();
-		
+
 		return {
 			x : event.clientX - offset.left + this.$window.scrollLeft(),
 			y : event.clientY - offset.top + this.$window.scrollTop()
@@ -99,7 +99,7 @@ var RENDERER = {
 	},
 	moveEpicenter : function(event){
 		var axis = this.getAxis(event);
-		
+
 		if(!this.axis){
 			this.axis = axis;
 		}
@@ -111,7 +111,7 @@ var RENDERER = {
 			return;
 		}
 		var index = Math.round(x / this.pointInterval);
-		
+
 		if(index < 0 || index >= this.points.length){
 			return;
 		}
@@ -119,7 +119,7 @@ var RENDERER = {
 	},
 	reverseVertical : function(){
 		this.reverse = !this.reverse;
-		
+
 		for(var i = 0, count = this.fishes.length; i < count; i++){
 			this.fishes[i].reverseVertical();
 		}
@@ -143,7 +143,7 @@ var RENDERER = {
 		this.controlStatus();
 		this.context.clearRect(0, 0, this.width, this.height);
 		this.context.fillStyle = 'hsl(0, 0%, 95%)';
-		
+
 		for(var i = 0, count = this.fishes.length; i < count; i++){
 			this.fishes[i].render(this.context);
 		}
@@ -151,7 +151,7 @@ var RENDERER = {
 		this.context.globalCompositeOperation = 'xor';
 		this.context.beginPath();
 		this.context.moveTo(0, this.reverse ? 0 : this.height);
-		
+
 		for(var i = 0, count = this.points.length; i < count; i++){
 			this.points[i].render(this.context);
 		}
@@ -171,7 +171,7 @@ SURFACE_POINT.prototype = {
 	SPRING_FRICTION : 0.9,
 	WAVE_SPREAD : 0.3,
 	ACCELARATION_RATE : 0.01,
-	
+
 	init : function(){
 		this.initHeight = this.renderer.height * this.renderer.INIT_HEIGHT_RATE;
 		this.height = this.initHeight;
@@ -218,13 +218,13 @@ var FISH = function(renderer){
 };
 FISH.prototype = {
 	GRAVITY : 0.4,
-	
+
 	init : function(){
 		this.direction = Math.random() < 0.5;
 		this.x = this.direction ? (this.renderer.width + this.renderer.THRESHOLD) : -this.renderer.THRESHOLD;
 		this.previousY = this.y;
 		this.vx = this.getRandomValue(4, 10) * (this.direction ? -1 : 1);
-		
+
 		if(this.renderer.reverse){
 			this.y = this.getRandomValue(this.renderer.height * 1 / 10, this.renderer.height * 4 / 10);
 			this.vy = this.getRandomValue(2, 5);
@@ -250,7 +250,7 @@ FISH.prototype = {
 		this.x += this.vx;
 		this.y += this.vy;
 		this.vy += this.ay;
-		
+
 		if(this.renderer.reverse){
 			if(this.y > this.renderer.height * this.renderer.INIT_HEIGHT_RATE){
 				this.vy -= this.GRAVITY;
@@ -279,7 +279,7 @@ FISH.prototype = {
 			this.phi %= Math.PI * 2;
 		}
 		this.renderer.generateEpicenter(this.x + (this.direction ? -1 : 1) * this.renderer.THRESHOLD, this.y, this.y - this.previousY);
-		
+
 		if(this.vx > 0 && this.x > this.renderer.width + this.renderer.THRESHOLD || this.vx < 0 && this.x < -this.renderer.THRESHOLD){
 			this.init();
 		}
@@ -294,7 +294,7 @@ FISH.prototype = {
 		context.bezierCurveTo(-20, 15, 15, 10, 40, 0);
 		context.bezierCurveTo(15, -10, -20, -15, -30, 0);
 		context.fill();
-		
+
 		context.save();
 		context.translate(40, 0);
 		context.scale(0.9 + 0.2 * Math.sin(this.theta), 1);
@@ -306,13 +306,13 @@ FISH.prototype = {
 		context.quadraticCurveTo(5, -10, 0, 0);
 		context.fill();
 		context.restore();
-		
+
 		context.save();
 		context.translate(-3, 0);
 		context.rotate((Math.PI / 3 + Math.PI / 10 * Math.sin(this.phi)) * (this.renderer.reverse ? -1 : 1));
-		
+
 		context.beginPath();
-		
+
 		if(this.renderer.reverse){
 			context.moveTo(5, 0);
 			context.bezierCurveTo(10, 10, 10, 30, 0, 40);
